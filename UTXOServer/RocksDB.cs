aff366195d7c066f65c9289bd6edc66405453785
Youngsun <lsun@live.cn>
2018-10-30 18:17:15 +0800

    Initial commit

---
+++ UTXOServer/RocksDB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UTXOServer {
+    class RocksDB {
+        public void Put(string key, string value) {
+
+        }
+
+        public void Del(string key) {
+
+        }
+
+        public void Get(string key) {
+
+        }
+    }
+}
