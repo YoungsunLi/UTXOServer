4bbb0b89a15382540fb71b53b0a051d667173264
Youngsun <lsun@live.cn>
2018-11-01 12:27:32 +0800

    接好接口

--- UTXOServer/RocksDB.cs
+++ UTXOServer/RocksDB.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using RocksDbSharp;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UTXOServer {
     class RocksDB {
+        readonly private DbOptions options = new DbOptions().SetCreateIfMissing(true);
+        readonly private string path = "db";//数据库位置
+
         /*
         Put(byte[] key, byte[] value);
         Put(string key, string value);
@@ -12,7 +16,7 @@ namespace UTXOServer {
         Get(byte[] key, byte[] buffer, long offset, long length);----------long
         Get(string key);----------string
         Get(byte[] key, long keyLength, byte[] buffer, long offset, long length);----------long
-        Get(byte[] key, long keyLength);####byte[]
+        Get(byte[] key, long keyLength);----------byte[]
         Get(byte[] key);----------byte[]
 
         Remove(byte[] key);
@@ -20,39 +24,65 @@ namespace UTXOServer {
         Remove(byte[] key, long keyLength);
         */
 
+        //Put
         public void Put(byte[] key, byte[] value) {
-
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                db.Put(key, value);
+            }
         }
         public void Put(string key, string value) {
-
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                db.Put(key, value);
+            }
         }
         public void Put(byte[] key, long keyLength, byte[] value, long valueLength) {
-
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                db.Put(key, keyLength, value, valueLength);
+            }
         }
+
+        //Get
         public long Get(byte[] key, byte[] buffer, long offset, long length) {
-            return 0;
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                return db.Get(key, buffer, offset, length);
+            }
         }
         public string Get(string key) {
-            return "";
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                return db.Get(key);
+            }
         }
         public long Get(byte[] key, long keyLength, byte[] buffer, long offset, long length) {
-            return 0;
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                return db.Get(key, keyLength, buffer, offset, length);
+            }
         }
         public byte[] Get(byte[] key, long keyLength) {
-            return null;
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                return db.Get(key, keyLength);
+            }
         }
         public byte[] Get(byte[] key) {
-            return null;
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                return db.Get(key);
+            }
         }
 
+        //Remove
         public void Remove(byte[] key) {
-
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                db.Remove(key);
+            }
         }
         public void Remove(string key) {
-
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                db.Remove(key);
+            }
         }
         public void Remove(byte[] key, long keyLength) {
-
+            using(RocksDb db = RocksDb.Open(options, path)) {
+                db.Remove(key, keyLength);
+            }
         }
     }
 }
