411fbecc6ee4df732232ac1e4c415ac8869bff9c
Youngsun <lsun@live.cn>
2018-11-02 16:07:35 +0800

    用错using, 去了using性能暴增

--- UTXOServer/RocksDB.cs
+++ UTXOServer/RocksDB.cs
@@ -1,13 +1,12 @@
 ﻿using RocksDbSharp;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using Newtonsoft.Json.Linq;
 
 namespace UTXOServer {
     class RocksDB {
-        readonly private DbOptions options = new DbOptions().SetCreateIfMissing(true);
-        readonly private string path = "db";//数据库位置
+        readonly private static DbOptions options = new DbOptions().SetCreateIfMissing(true);
+        readonly private static string path = "D://dbtest";//数据库位置
+        readonly private static RocksDb db = RocksDb.Open(options, path);
+
 
         /*
         Put(byte[] key, byte[] value);
@@ -26,81 +25,53 @@ namespace UTXOServer {
         */
 
         //Put
-        public void Put(string txid, string n, string blockIndex, string asset, string address, string value) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                string key = txid + n;
-                string utxo = "{\"blockIndex\":\"" + blockIndex + "\",\"asset\":\"" + asset + "\",\"address\":\"" + address + "\",\"value\":\"" + value + "\"}";
-                db.Put(key, utxo);
-            }
+        public void Put(string txid, int n, string blockIndex, string asset, string address, string value) {
+            string key = txid + n;
+            string utxo = "{\"blockIndex\":\"" + blockIndex + "\",\"asset\":\"" + asset + "\",\"address\":\"" + address + "\",\"value\":\"" + value + "\"}";
+            db.Put(key, utxo);
         }
         public void Put(byte[] key, byte[] value) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                db.Put(key, value);
-            }
+            db.Put(key, value);
         }
         public void Put(string key, string value) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                db.Put(key, value);
-            }
+            db.Put(key, value);
         }
         public void Put(byte[] key, long keyLength, byte[] value, long valueLength) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                db.Put(key, keyLength, value, valueLength);
-            }
+            db.Put(key, keyLength, value, valueLength);
         }
 
         //Get
         public JObject Get(string txid, string n) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                return JObject.Parse(db.Get(txid + n));
-            }
+            return JObject.Parse(db.Get(txid + n));
         }
         public long Get(byte[] key, byte[] buffer, long offset, long length) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                return db.Get(key, buffer, offset, length);
-            }
+            return db.Get(key, buffer, offset, length);
         }
         public string Get(string key) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                return db.Get(key);
-            }
+            return db.Get(key);
         }
         public long Get(byte[] key, long keyLength, byte[] buffer, long offset, long length) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                return db.Get(key, keyLength, buffer, offset, length);
-            }
+            return db.Get(key, keyLength, buffer, offset, length);
         }
         public byte[] Get(byte[] key, long keyLength) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                return db.Get(key, keyLength);
-            }
+            return db.Get(key, keyLength);
         }
         public byte[] Get(byte[] key) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                return db.Get(key);
-            }
+            return db.Get(key);
         }
 
         //Remove
-        public void Remove(string txid, string n) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                db.Remove(txid + n);
-            }
+        public void Remove(string txid, int n) {
+            db.Remove(txid + n);
         }
         public void Remove(byte[] key) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                db.Remove(key);
-            }
+            db.Remove(key);
         }
         public void Remove(string key) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                db.Remove(key);
-            }
+            db.Remove(key);
         }
         public void Remove(byte[] key, long keyLength) {
-            using(RocksDb db = RocksDb.Open(options, path)) {
-                db.Remove(key, keyLength);
-            }
+            db.Remove(key, keyLength);
         }
     }
 }
